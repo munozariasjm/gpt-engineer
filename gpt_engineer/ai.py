b9196012321280a5616280d16a1fb652f8bb0716
Jose M Munoz <munozariasjm@gmail.com>
2023-06-28 08:30:56 -0500

    Use POECALLER instead of Openai's one

--- gpt_engineer/ai.py
+++ gpt_engineer/ai.py
@@ -1,21 +1,27 @@
 from __future__ import annotations
-
+import os
 import logging
-
-import openai
+import json
+from poecaller import LangPoeCaller as PoeCaller
 
 logger = logging.getLogger(__name__)
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+SECTRETS = f"{dir_path}/../secrets.json"
+with open(SECTRETS) as f:
+    secrets = json.load(f)
+
 
 class AI:
-    def __init__(self, model="gpt-4", temperature=0.1):
-        self.temperature = temperature
-        self.model = model
+    def __init__(self, model="chinchilla", **kwargs):
+        self.bot_codename = model
+        self.poe_caller = PoeCaller(secrets["POE_TOKEN"])
 
     def start(self, system, user):
         messages = [
-            {"role": "system", "content": system},
-            {"role": "user", "content": user},
+            self.fsystem(system),
+            self.fuser(user),
         ]
 
         return self.next(messages)
@@ -31,36 +37,24 @@ class AI:
 
     def next(self, messages: list[dict[str, str]], prompt=None):
         if prompt:
-            messages += [{"role": "user", "content": prompt}]
+            messages += [self.fuser(prompt)]
 
         logger.debug(f"Creating a new chat completion: {messages}")
-        response = openai.ChatCompletion.create(
-            messages=messages,
-            stream=True,
-            model=self.model,
-            temperature=self.temperature,
+        responses = self.poe_caller.chat(
+            self.bot_codename,
+            [msg["content"] for msg in messages if msg["role"] == "user"],
         )
 
         chat = []
-        for chunk in response:
-            delta = chunk["choices"][0]["delta"]
-            msg = delta.get("content", "")
-            print(msg, end="")
-            chat.append(msg)
+        for response in responses:
+            print(response, end="")
+            chat.append(response)
         print()
-        messages += [{"role": "assistant", "content": "".join(chat)}]
+        messages += [self.fassistant("".join(chat))]
         logger.debug(f"Chat completion finished: {messages}")
         return messages
 
 
 def fallback_model(model: str) -> str:
-    try:
-        openai.Model.retrieve(model)
-        return model
-    except openai.InvalidRequestError:
-        print(
-            f"Model {model} not available for provided API key. Reverting "
-            "to gpt-3.5-turbo. Sign up for the GPT-4 wait list here: "
-            "https://openai.com/waitlist/gpt-4-api\n"
-        )
-        return "gpt-3.5-turbo"
+    assert model in ["chinchilla", "a2"], f"Unknown model: {model}"
+    return model
